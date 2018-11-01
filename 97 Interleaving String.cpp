3428673e47343d54caa9e0547912ba9509c36e17
Benjamin Chang <benjamin.30805@yahoo.com.tw>
2018-11-01 22:00:25 +0800

    20181101 把這題改為標準解答的演算法

--- 97 Interleaving String.cpp
+++ 97 Interleaving String.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        bool re = 0;
         if(s1.size() + s2.size() != s3.size())
-            return 0;
-        if(s1.empty())
-            return s2 == s3;
-        if(s2.empty())
-            return s1 == s3;
+            return false;
+        if(s1.empty() || s2.empty())
+            return s1+s2==s3;
+        bool arr[s1.size()+1][s2.size()+1];
+        memset(arr, false, sizeof(arr));
 
-        if(!re && s1[0] == s3[0] && s1[s1.size()-1] == s3[s3.size()-1]){
-            re |= isInterleave(s1.substr(1, s1.size()-2), s2, s3.substr(1, s3.size()-2));
-        }//1前後
-        if(!re && s2[0] == s3[0] && s2[s2.size()-1] == s3[s3.size()-1]){
-            re |= isInterleave(s1, s2.substr(1, s2.size()-2), s3.substr(1, s3.size()-2));
-        }//2前後
-        if(!re && s1[0] == s3[0] && s2[s2.size()-1] == s3[s3.size()-1]){
-            re |= isInterleave(s1.substr(1, s1.size()), s2.substr(0, s2.size()-1), s3.substr(1, s3.size()-2));
-        }//1前2後
-        if(!re && s2[0] == s3[0] && s1[s1.size()-1] == s3[s3.size()-1]){
-            re |= isInterleave(s2.substr(1, s2.size()), s1.substr(0, s1.size()-1), s3.substr(1, s3.size()-2));
-        }//2前1後
-        return re;
+        arr[0][0] = true;
+        for(int i = 1;arr[i-1][0] && i < s1.size()+1;++i){
+            arr[i][0] = (s1[i-1] == s3[i-1]);
+        }
+        for(int i = 1;arr[0][i-1] && i < s2.size()+1;++i){
+            arr[0][i] = (s2[i-1] == s3[i-1]);
+        }
+        int i = 1, j = 1;
+        for(;i < s1.size()+1;++i){
+            for(j = 1;j < s2.size()+1;++j){
+                arr[i][j] = (arr[i-1][j] && s3[i+j-1] == s1[i-1]) || 
+                            (arr[i][j-1] && s3[i+j-1] == s2[j-1]);
+            }
+        }
+        return arr[i-1][j-1];
     }
 };
