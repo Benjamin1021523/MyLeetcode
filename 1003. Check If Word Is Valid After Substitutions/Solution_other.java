57ec6adc1fb60ea8caebc6c05cbb9e05ddfd5fcd
Benjamin1021523 <yzu1021523@gmail.com>
2023-07-01 00:28:10 +0800

    1003題其他方法1，使用map記錄目標增加擴展性，但有使用限制

---
+++ 1003. Check If Word Is Valid After Substitutions/Solution_other.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValid(String s) {
+        List<Integer> list = new ArrayList<>();
+
+        boolean ans = true;
+        for (char c : s.toCharArray()) {
+            if (!ans) break;
+            ans &= func(list, c);
+        }
+        return ans && list.isEmpty();
+    }
+
+    private static final Map<Character, Integer> map = new HashMap<>() {{
+        int i = 0;
+        for (char c1 : "abc".toCharArray()) {
+            put(c1, i++);
+        }
+    }};
+
+    private boolean func(List<Integer> list, char c) {
+        if (map.get(c) == 0) {
+            list.add(0);
+        } else {
+            if (list.isEmpty() || !Objects.equals(list.get(list.size() - 1), map.get(c) - 1)) {
+                return false;
+            } else if (map.get(c) == 2) {
+                list.remove(list.size() - 1);
+            } else {
+                list.set(list.size() - 1, map.get(c));
+            }
+        }
+
+        return true;
+    }
+}
