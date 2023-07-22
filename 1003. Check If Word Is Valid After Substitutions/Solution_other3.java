bd21653e5ad8a329ce956ab772d5ec90903e18f7
Benjamin1021523 <yzu1021523@gmail.com>
2023-07-22 10:37:40 +0800

    1003題其他方法3，試著解釋討論區的超快解法

---
+++ 1003. Check If Word Is Valid After Substitutions/Solution_other3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] cs = s.toCharArray();
+        int i = 0;
+        for (char c : cs) {
+            if (c != 'a') {
+                if (i == 0) {
+                    return false;
+                } else {
+                    --i;
+                    if (c != cs[i] + 1) {// 字元非a的情況一定要是b或c，前一個應為對應的a或b
+                        return false;
+                    }
+                }
+            }
+
+            if (c != 'c') {
+                cs[i] = c;
+                ++i;
+            }
+        }
+        return i == 0;
+    }
+}
