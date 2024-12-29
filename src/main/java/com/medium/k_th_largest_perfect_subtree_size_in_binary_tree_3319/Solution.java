1c99d3e9de45de001e08716f48ac3a1571582ed3
benjamin <benjaminchang@crm.com.tw>
2024-12-29 15:41:43 +0800

    3319第二版，不使用SortedMap，純粹由大到小檢查所有大小的數量

--- src/main/java/com/medium/k_th_largest_perfect_subtree_size_in_binary_tree_3319/Solution.java
+++ src/main/java/com/medium/k_th_largest_perfect_subtree_size_in_binary_tree_3319/Solution.java
@@ -4,26 +4,24 @@ import java.util.*;
 
 public class Solution extends AbstractSolution {
     public int kthLargestPerfectSubtree(TreeNode root, int k) {
-        result = new TreeMap<>(Comparator.reverseOrder());
-        perfectTreeCount = 0;
-
         countTreeNodesDepth(root);
 
         if (k > perfectTreeCount)
             return -1;
 
-        for (Integer key : result.keySet()) {
-            k -= result.get(key);
+        for (int depth = nowMaxDepth; depth > 0; depth--) {
+            k -= result.get(depth);
             if (k <= 0) {
-                return (int) Math.pow(2, key) - 1;
+                return (int) Math.pow(2, depth) - 1;
             }
         }
 
         return -1;
     }
 
-    private SortedMap<Integer, Integer> result;
-    private int perfectTreeCount;
+    private final Map<Integer, Integer> result = new HashMap<>();
+    private int nowMaxDepth = -1;
+    private int perfectTreeCount = 0;
     private int countTreeNodesDepth(TreeNode root) {
         if (root == null)
             return 0;
@@ -36,6 +34,9 @@ public class Solution extends AbstractSolution {
 
         result.merge(nowDepth, 1, Integer::sum);
         perfectTreeCount++;
+        if (nowDepth > nowMaxDepth) {
+            nowMaxDepth = nowDepth;
+        }
 
         return nowDepth;
     }
